015f78e84ed00483ed776a7b152098fc52a00dc6
Robert Showalter <robert@orangedaisy.co>
2018-02-22 18:36:25 -0500

    Create 'home' partial and add basic theme styles
    - Add initial theme styling based on Figma mockups. More refactoring is
      coming based on future needs. For now, we're just worried about
      getting the themed "greeting card" up on production.
    - Modify countdown script to only show days.

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -4,30 +4,26 @@ const weddingDay = '2018-06-09';
 
 function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date());
-    let minutes = Math.floor((t / 1000 / 60) % 60);
-    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     let days = Math.floor(t / (1000 * 60 * 60 * 24));
 
     return {
         'total': t,
         'days': days,
-        'hours': hours,
-        'minutes': minutes
     };
 }
 
 function initializeClock(element, endtime) {
     let clock = document.querySelector(element);
-    let days = clock.querySelector('.js-countdown__days');
-    let hours = clock.querySelector('.js-countdown__hours');
-    let minutes = clock.querySelector('.js-countdown__minutes');
+    let daysRemaining = clock.querySelector('.js-countdown__days');
 
     function updateClock() {
         let t = getTimeRemaining(endtime);
 
-        days.innerHTML = t.days;
-        hours.innerHTML = t.hours;
-        minutes.innerHTML = t.minutes;
+        if (t.total > 1) {
+            daysRemaining.innerHTML = `${t.days} days`;
+        } else if (t.total === 1) {
+            daysRemaining.innerHTML = `${t.days} day`;
+        }
 
         if (t.total <= 0) {
             clearInterval(timeInterval);
